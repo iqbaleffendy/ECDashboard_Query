d712789f18d0f0975ef1206e98c53c031506fad7
iqbaleffendy <miqbaleffendy18@gmail.com>
2021-12-20 17:50:39 +0700

    Modifying query structure

--- EC_Invoice.sql
+++ EC_Invoice.sql
@@ -1,317 +1,9 @@
 delete from EDW_ANALYTICS.CRM.EC_fact_invoice where format(MTD,'yyyyMM') = format(CURRENT_TIMESTAMP, 'yyyyMM');
 --delete from EDW_ANALYTICS.CRM.EC_fact_invoice where format(MTD,'yyyyMM') = '202111';
 
---CTE Forecast Daily
-with forecaseDaily as(
-select 
-	oppt.LEAD_ID LeadID, 
-	oppt.OPPORTUNITY_ID OpportunityID, 
-	oppt.ITEM_NO OpportunityItemNo,
-	oppt.QUOTATION_ID QuotationID,
-	oppt.ITEM_NO_QUOTATION QuotationItemNo,
-	oppt.SO_ID SOID,
-	oppt.ITEM_NO_SO SOItemNo,
-    CONVERT(date, CAST(oppt.LEAD_DATE_KEY AS varchar)) as LeadCreatedDate, 
-    CONVERT(date, CAST(oppt.LEAD_CHANGED_DATE_KEY AS varchar)) as LeadChangedDate,
-	slo.[DESCRIPTION] as SourceofLead,
-    CONVERT(date, CAST(oppt.OPP_DATE_KEY AS varchar)) as OppIDCreatedDate,
-    CONVERT(date, CAST(oppt.OPP_CHANGED_DATE_KEY AS varchar)) as OppIDChangedDate,
-    CONVERT(date, CAST(oppt.OPP_ITEM_DATE_KEY AS varchar)) as OppItemCreatedDate,
-    CONVERT(date, CAST(oppt.OPP_ITEM_CHANGED_DATE_KEY AS varchar)) as OppItemChangedDate,
-	CONVERT(date, CAST(oppt.QUOT_CREATED_DATE_KEY AS varchar)) as QuotationIDCreatedDate, 
-    CONVERT(date, CAST(oppt.QUOT_CHANGED_DATE_KEY AS varchar)) as QuotationIDChangedDate,
-    CONVERT(date, CAST(oppt.QUOT_ITEM_CREATED_DATE_KEY AS varchar)) as QuotationItemCreatedDate, 
-    CONVERT(date, CAST(oppt.QUOT_ITEM_CHANGED_DATE_KEY AS varchar)) as QuotationItemChangedDate,
-	CONVERT(date, CAST(oppt.SO_CREATED_DATE_KEY AS varchar)) as SOIDCreatedDate,
-    CONVERT(date, CAST(oppt.BILLING_CREATED_DATE AS varchar)) as BillingIDCreatedDate,
-	oppt.TRANSACTION_DESC TransactionDescription,
-	oppt.customer_key,
-	dc.CUSTOMER_CODE as AccountID,
-	dc.FULL_NAME as AccountName,
-	cic.CIC_Group CICGroup,
-	cic.CIC_Description CICDescription,
-	dsl.AREA_NAME AS SalesOfficeArea,
-	dsl.STORE_ABBREVATION_NAME + ' - ' + dsl.STORE_NAME as SALES_OFFICE,
-	dsl.store_abbrevation_name sales_off_code,
-	dsl.STORE_ABBREVATION_NAME AS SalesOffice,
-	dsl.COMPANY_NAME,
-	dsl.SUB_COMPANY_NAME,
-	dsl.REGION_NAME,
-	case when dse.occupation_code = 'SE' then 
-	dse.FULL_NAME else 
-	dsp.FULL_NAME end as SalesRepsName,
-		case when dse.occupation_code = 'SE' then oppt.SALES_EXECUTIVE_KEY else oppt.SALES_PERSON_KEY end as SALES_KEY, 
-	case when dse.occupation_code = 'SE' then 
-	dse.OCCUPATION_CODE else 
-	dsp.OCCUPATION_CODE end as OccupationCode,
-	opst.[DESCRIPTION] as OpportunityStatus,
-    CONVERT(date, CAST(oppt.OPPORTUNITY_STATUS_DATE_KEY AS varchar)) as OpportunityStatusDate,
-    dqs.[DESCRIPTION] as QuotationStatus,
-	dss.[DESCRIPTION] AS SOItemStatus,
-	drj.Rejection_Reason as SORejectionReason,
-    optp.OPPORTUNITY_TYPE_DESCRIPTION as OpportunityType, 
-	dpm.PRODUCT_MATERIAL_CODE ProductID,
-	dpm.PRODUCT_MODEL ProductModel,
-	dpm.[DESCRIPTION] as ProductDescription,
-	dpm.VALID_MATERIAL ValidMaterial,
-	sn.Serial_No SerialNo,
-	sn.Batch_ID BatchID,
-	oppt.MARKET_SECTOR_KEY as market_sector_key,
-	dms.MARKET_SECTOR_CODE PWC,
-	dms.[DESCRIPTION] as MarketDescription,
-	dms.INDUSTRY_GROUP,
-	dms.PRODUCT_DIVISION,
-	dms.INDUSTRY_SEGMENT,
-	case when dms.[INDUSTRY_GROUP] is not null then dms.[INDUSTRY_GROUP] else dms.[INDUSTRY_SEGMENT] end as MarketSector,
-	dpd.[DESCRIPTION] as POINT_OF_DELIVERY_DESC,
-    CONVERT(date, CAST(oppt.DELIVERY_DATE_KEY AS varchar)) as deliverydate,
-	oppt.net_value NetValueInUSD,
-    oppt.net_value NetValueInIDR,
-	oppt.EXPECTED_TOTAL_VALUE ExpectedTotalVaue,
-	curr.CURRENCY_CODE Currency,
-	oppt.QUOT_NET_VALUE QuotNetValue,
-	currq.CURRENCY_CODE as QOUT_CURRENCY_CODE,
-	flag.[DESCRIPTION] as FORECAST,
-	da.Major_Account_Classification MajorAccountClassification,
-	da.Customer_Class_Code CustomerClassCode,
-	da.INDUSTRY_KEY,
-	da.Vertical_Industry VerticalIndustry,
-	oppt.product_material_key,
-    dpm.material_category MaterialType,
-	dpm.Category_ID,
-	oppt.CONFIDENCE_LEVEL ConfidenceLevel, 
-	dpm.BASIC_SELLING_PRICE_VALID_FROM BasicSellingPriceValidFrom,
-	dpm.BASIC_SELLING_PRICE_VALID_TO BasicSellingPriceValidTo,
-	dpmm.product_hierarchy,
-	dps.sales_type,
-	CONVERT(date, CAST(oppt.REPORT_DATE_KEY AS varchar)) as LOAD_DATE
-from
-[LS_BI_PROD].EDW_CRM_ANALYTICS.dbo.FACT_CRM_OPPORTUNITY oppt left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_OPPORTUNITY_TYPE optp on (oppt.OPPORTUNITY_TYPE_KEY = optp.OPPORTUNITY_TYPE_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_USER_STATUS opst on (oppt.USER_STATUS_KEY = opst.USER_STATUS_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.dbo.DIM_FLAG_YN flag on (oppt.FLAG_FORECAST_KEY = flag.FLAG_YN_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_SALES_PERSON dsp on (oppt.SALES_PERSON_KEY = dsp.SALES_PERSON_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_SALES_PERSON dse on (oppt.SALES_EXECUTIVE_KEY = dse.SALES_PERSON_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_SALES_LOCATION dsl on (oppt.SALES_LOCATION_KEY = dsl.SALES_LOCATION_KEY) left join
-(select case when isnumeric(product_material_code)=1 then cast(CAST(product_material_code as int) as nvarchar) else product_material_code end material_code
-, * from [LS_BI_PROD].EDW_DIMENSION.CRM.DIM_PRODUCT_MATERIAL) dpm on (oppt.PRODUCT_MATERIAL_KEY = dpm.PRODUCT_MATERIAL_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_MARKET_SECTOR dms on (oppt.MARKET_SECTOR_KEY = dms.MARKET_SECTOR_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_SO_STATUS dss on (oppt.SO_STATUS_KEY = dss.SO_STATUS_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.Dim_Rejection drj on (oppt.SO_REJECTION_KEY = drj.Rejection_key) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_POINT_OF_DELIVERY dpd on (oppt.POINT_OF_DELIVERY_KEY = dpd.POINT_OF_DELIVERY_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_QUOTATION_STATUS dqs on (oppt.QUOTATION_STATUS_KEY = dqs.QUOTATION_STATUS_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_CUSTOMER dc on (oppt.CUSTOMER_KEY = dc.CUSTOMER_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.Dim_CIC cic on (oppt.CIC_KEY = cic.CIC_KEY) left join 
-[LS_BI_PROD].EDW_DIMENSION.ECC.Dim_Serial_Equi sn on (oppt.SerialEqui_Key = sn.SerialEqui_Key) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_CURRENCY curr on (oppt.CURRENCY_KEY = curr.CURRENCY_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_CURRENCY currq on (oppt.QUOT_CURRENCY_KEY = currq.CURRENCY_KEY) left join
-[LS_BI_PROD].EDW_DIMENSION.CRM.DIM_ORIGIN_LEAD_OPP slo on (oppt.LEAD_ORIGIN_KEY = slo.ORIGIN_OPP_LEAD_KEY) left join
-[LS_BI_PROD].EDW_ANALYTICS.ECC.dim_account da on (case when dc.CUSTOMER_CODE < 0 then dc.CUSTOMER_CODE else RIGHT('00000' + CAST(dc.CUSTOMER_CODE as varchar(10)), 10) end = da.account_id) left join
-[LS_BI_PROD].EDW_ANALYTICS.crm.dim_price_scenario dps on oppt.PRICE_SCENARIO = dps.price_scenario_key
-left join (select case when isnumeric(product_material_code)=1 then cast(CAST(product_material_code as int) as nvarchar)  else product_material_code end material_code
-, * from [LS_BI_PROD].EDW_ANALYTICS.CRM.dim_opp_product_material) dpmm on dpm.material_code = dpmm.material_code
-where
-            optp.OPPORTUNITY_TYPE_DESCRIPTION not in ('N/A', 'Unknown', 'LEAD')
-            and ( opst.[DESCRIPTION] like '%stage 3%' or
-            opst.[DESCRIPTION] like '%stage 4%' or
-            opst.[DESCRIPTION] like '%stage 5%' or
-            opst.[DESCRIPTION] like '%stage 6%' or
-            opst.[DESCRIPTION] like '%won%' or
-            opst.[DESCRIPTION] like '%delivered%' or
-            opst.[DESCRIPTION] like '%lost%')
-			and left(oppt.LEAD_DATE_KEY, 4) in (YEAR(GETDATE()), YEAR(GETDATE())-1)
-            
-)
---CTE Order Tracking Daily
-,invoice as (
-select 
-		cast(VBELN_VBAP as int) SalesDocument, 
-		POSNR_VBAP SalesDocumentItem,
-		AUART SalesDocumentType,
-		--uepos HigerLevelItem,
-		BSTKD_E SpaNo,
-		a.BSTKD PurchaseOrderNo,
-		BSTDK PurchaseOrderDate,
-		a.VKORG SalesOrganization,
-		--a.SPART Division,
-		a.AREA Area,
-		a.VKBUR SalesOffice,
-		a.KUNNR_VBAK SoldToParty,
-		a.Gtext EndDestination,
-		--a.Name1_bilpar BilltoPartyname,
-		a.KUNNR_PAYER Payer,
-		a.Name2_Payer Payername,
-		a.Kunnr_finance FinancingCompany,
-		a.name2_finance FinancingCompanyName,
-		a.VTEXT_t179T Model,
-		b.BWTAR Batch,
-		b.SERNR serialNumber,
-		b.MFRPN MaterialNumber,
-		a.Arktx MaterialDescription,
-		a.HERKL Source,
-		a.INCO1 Incoterms,
-		a.INCO2 IncotermsPart2,
-		a.MVGR2 PWCCode,
-		a.MVGR3 ApplicationCode,
-		cic.cic_group CICGroup,
-		a.DELDATE RequestDeliveryDate,
-		--a.Payment TermsofPayment,
-		--a.PAYMENTNOTE PaymentTermsNote,
-		a.VTEXT Description,
-		a.NETWR Price,
-		a.MWSBP Tax ,
-		--a.WAERK PDC_PDG_curr,
-		a.SALID SalesmanID,
-		a.SALNAME SalesmanName,
-		--a.DSALID DeliverySalesmanID,
-		--a.DSALNAME DeliverySalesmanName,
-		--a.MKTPGM Marketingprogram,
-		--a.VCHR Voucher,
-		--a.CCR CCRNo,
-		a.RLAPNO ReleaseApprovalNo,
-		a.RLAPDT ReleaseApprovalDate,
-		a.RLAPBY ReleaseApprovalBy,
-		 case when a.Created='X' THEN 'Yes' else 'No' end Created,
-		 case when a.REQSTAT='X' THEN 'Yes' else 'No' end RequestChangeStatustoWorkable,
-		 case when a.WOrkable='X' THEN 'Yes' else 'No' end Workable,
-		 case when a.REQODCHK='X' THEN 'Yes' else 'No' end RequestToDoOverdueCheck,
-		 case when a.REQRLAPNO='X' THEN 'Yes' else 'No' end RequestReleaseApprovalNo,
-		 case when a.RELAPPR='X' THEN 'Yes' else 'No' end ReleaseApproval,
-		 case when a.FULLY='X' THEN 'Yes' else 'No' end FullyPaid,
-		 case when po.ZZLEASING_STAT = 'X' THEN 'Yes' else 'No' end POLeasing,
-		--a.NOTEBR NotesforOverdueBranch,
-		--a.NOTEGRP NotesforOverdueGroup,
-		a.REMARKS Remarks,
-		a.STCD1 NPWP,
-		a.VBELN_LIPS Delivery,
-		a.BLDAT DeliveryDate,
-		a.BAST_NO BAST_NO,
-		a.WBSTK StatusPGI,
-		a.WADAT_IST PGIdate,
-		a.E_DATLO BASTSigndate ,
-		a.PDSTK ProofOfdeliveryStatus,
-		a.PODAT ProofOfdeliverydate,
-		CONVERT(BIGINT,b.BILLING_DOC) BillingDocument,
-		b.BILLING_DATE  BillingDate,
-		--a.ZREJECT_RSN ReasonforRejection,
-		--a.PDC_PDG PDC_PDG,
-		a.Amount Amount ,
-		a.DUE_DATE DueDate,
-		--a.VTWEG DistributionChannel,
-		--a.Region Region,
-		a.NAME1_SOl SoldToPartyName ,
-		a.GSBER BusinessArea,
-		--a.TELF1 CustomerPhoneNo,
-		--a.Name_ser ServiceContact,
-		--a.NAME_CON SalesContact,
-		a.KUNNR_BILPAR BillToParty,
-		a.KWMENG Quantity,
-		a.CURR_So SOCurr,
-		a.dat_lease LeasingFundingDate,
-		a.dat_Sppka SPPKADate,
-		b.ACTUAL_NETREV ActualNetRevenue,
-		a.DAT_LEASE SODeliveryDate,
-		b.Division typeDivision,
-		b.mvgr1 market_sector,
-		null DeliveryFlag ,
-		null PP_ST3_ST5 ,
-		null ProgressStatus ,
-		b.ACTUAL_NETREV * (ABS(d.kurrf)) ActualNetInIDR,
-		b.ACTUAL_NETREV ActualNetInUSD,
-		da.customer_class_code CustomerClassCodes,
-		a.MATNR_VBAP as ProductCode,
-		dpm.PRODUCT_HIERARCHY
-		-- into EDW_ANALYTICS.dbo.invoice
-		from [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_V_ODRPP_BW a left join 
-		[LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_F_INV_SOLD b on a.VBELN_VBAP = b.VBELN and a.POSNR_VBAP =b.POSNR
-		left join [LS_BI_PROD].EDW_ANALYTICS.ECC.dim_account da on da.account_id = a.KUNNR_VBAK
-		left join [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.VBRK d on b.BILLING_DOC = d.VBELN
-		left join [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_V_SO_OPPTY po on cast(VBELN_VBAP as int) = po.SO_NUMBER and POSNR_VBAP = po.SO_ITEM
-		left join (select DISTINCT cic_group_id,cic_group from ls_bi_prod.EDW_DIMENSION.CRM.Dim_CIC) cic on cic.cic_group_id = b.brsch
-		left join (select * from [LS_BI_PROD].EDW_ANALYTICS.CRM.dim_opp_product_material where product_material_code not in ('N/A', 'Unknown') and left(product_hierarchy, 2) in ('M1','E1','F1')) dpm on CAST(a.MATNR_VBAP AS INT) = CAST (dpm.PRODUCT_MATERIAL_CODE AS INT)
-		where 1=1
-		and format(a.DELDATE,'yyyy') between (format(CURRENT_TIMESTAMP,'yyyy') - 1) and (format(CURRENT_TIMESTAMP,'yyyy') + 1)
-		--and format(a.DELDATE,'yyyyMM') in (format(DATEADD(MONTH, -1, CURRENT_TIMESTAMP),'yyyyMM'), format(CURRENT_TIMESTAMP,'yyyyMM'))
-)
---CTE Logscore
-,logscore as (
-select 
-	b.sernr,
-	b.CUST_NAME,
-	a.vbrk_fkdat, 
-	b.billing_date, 
-	ScoreLog.SCORE_DATE,
-	case when ScoreLog.SCORE_DATE < DATEADD(DAY,1,EOMONTH(b.Billing_Date)) and format(b.billing_date, 'yyyy-MM') <> format(ScoreLog.SCORE_DATE, 'yyyy-MM') and ScoreLog.CUST_NAME_OR_RENT_CUST_NAME = b.CUST_NAME then 0
-		when ScoreLog.SCORE_DATE  < DATEADD(DAY,1,EOMONTH(b.Billing_Date)) and format(b.billing_date, 'yyyy-MM') <> format(ScoreLog.SCORE_DATE, 'yyyy-MM') and ScoreLog.CUST_NAME_OR_RENT_CUST_NAME <> b.CUST_NAME then 1
-	else 1 end hitung
 
-from [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_V_ODRPP_BW a
-left join [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_F_INV_SOLD b on a.VBELN_VBAP = b.VBELN and a.POSNR_VBAP =b.POSNR 
-left join(
-	select sl.*, sn.material_no, pm.product_model
-	from (
-		select SCORE_DATE,PRIME_PRODUCT_SERIAL_NUMBER, CUSTOMER_CODE, CUST_NAME_OR_RENT_CUST_NAME, SCORE_ERROR, 'M1' AS MATERIAL_TYPE, SALE_TYPE from [LS_BI_PROD].EDW_STG_SAP_CRM_DAILY.dbo.zldb_score_mach union
-		select SCORE_DATE,PRIME_PRODUCT_SERIAL_NUMBER, CUSTOMER_CODE, CUST_NAME_RENT_CUST_NAME, SCORE_ERROR, 'E1' AS MATERIAL_TYPE, SALE_TYPE from [LS_BI_PROD].EDW_STG_SAP_CRM_DAILY.dbo.zldb_score_engn
-	) sl
-	left join (
-		select SERIAL_NO,  cast(material_no as int) material_no 
-		from [LS_BI_PROD].EDW_DIMENSION.ECC.Dim_Serial_Equi 
-		where material_no not in ('Not Available', 'Unknown') and left(material_no,9) <> '000000005'
-	) sn on sl.PRIME_PRODUCT_SERIAL_NUMBER = sn.SERIAL_NO
-	left join (
-		select product_model,  cast(product_material_code as int) product_material_code 
-		from [LS_BI_PROD].EDW_ANALYTICS.CRM.dim_opp_product_material
-		where product_hierarchy is not null
-	) pm on sn.material_no = pm.product_material_code
-) as ScoreLog
-on b.SERNR = ScoreLog.PRIME_PRODUCT_SERIAL_NUMBER
-where 1=1 
-and FORMAT (b.billing_date, 'yyyy') >= format(CURRENT_TIMESTAMP, 'yyyy')  
-and ScoreLog.SALE_TYPE  in (1,3,5) 
---and ScoreLog.SCORE_ERROR = 'N'
-)
---CTE Order Tracking ST3/ST5
-,st3 as (
-select 
-	a.SALE_TYPE, 
-	a.PRIME_PRODUCT_SERIAL_NUMBER,
-	'MACHINE' MaterialType,
-	c.sales_code,
-	c.area_name Area_Name, 
-	b.*,
-	dpm.PRODUCT_HIERARCHY,
-	a.SCORE_DATE 
-	from [LS_BI_PROD].EDW_STG_SAP_CRM_DAILY.dbo.zldb_score_mach a
-left join [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_V_ODRRUE_BW b 
-on a.PRIME_PRODUCT_SERIAL_NUMBER = b.sernr
-left join EDW_ANALYTICS.CRM.EC_dim_area_store c 
-on b.vkbur = c.sales_code
-left join (select * from [LS_BI_PROD].EDW_ANALYTICS.CRM.dim_opp_product_material where product_material_code not in ('N/A', 'Unknown') and left(product_hierarchy, 2) in ('M1','E1','F1')) dpm
-on CAST(b.MATNR AS INT) = CAST (dpm.PRODUCT_MATERIAL_CODE AS INT)
-where a.SCORE_ERROR = 'N' and a.SALE_TYPE in (3,5) and b.release_apprv = 'X'
---and format(a.SCORE_DATE,'yyyyMM') in (format(DATEADD(MONTH, -1, CURRENT_TIMESTAMP),'yyyyMM'), format(CURRENT_TIMESTAMP,'yyyyMM'))
-union
-select 
-	a.SALE_TYPE, 
-	a.PRIME_PRODUCT_SERIAL_NUMBER,
-	'ENGINE' MaterialType,
-	c.sales_code,
-	c.area_name Area_Name, 
-	b.*,
-	dpm.PRODUCT_HIERARCHY,
-	a.SCORE_DATE 
-	from [LS_BI_PROD].EDW_STG_SAP_CRM_DAILY.dbo.zldb_score_engn a
-left join [LS_BI_PROD].EDW_STG_SAP_ECC_DAILY.dbo.ZLDB_V_ODRRUE_BW b 
-on a.PRIME_PRODUCT_SERIAL_NUMBER = b.sernr
-left join EDW_ANALYTICS.CRM.EC_dim_area_store c 
-on b.vkbur = c.sales_code
-left join (select * from [LS_BI_PROD].EDW_ANALYTICS.CRM.dim_opp_product_material where product_material_code not in ('N/A', 'Unknown') and left(product_hierarchy, 2) in ('M1','E1','F1')) dpm
-on CAST(b.MATNR AS INT) = CAST (dpm.PRODUCT_MATERIAL_CODE AS INT)
-where a.SCORE_ERROR = 'N' and a.SALE_TYPE in (3,5) and b.release_apprv = 'X'
---and format(a.SCORE_DATE,'yyyyMM') in (format(DATEADD(MONTH, -1, CURRENT_TIMESTAMP),'yyyyMM'), format(CURRENT_TIMESTAMP,'yyyyMM'))
-)
 --CTE for Invoiced PP
-,invoiced as (
+with invoiced as (
 select 
 	distinct a.*,
 	case 
@@ -385,13 +77,13 @@ select
 	da.Customer_Group
 	--,CONCAT(CAST(i.SoldToParty as INT),i.ProductHierarchy, year(i.BillingDate), month(i.BillingDate)) AS forecastedkey
 
-from invoice i 
-left join forecaseDaily f on i.salesDocument = f.SOID and i.salesdocumentItem = f.SOItemNo  
+from EDW_ANALYTICS_STG.CRM.stg_EC_ordertrackingPP i 
+left join EDW_ANALYTICS_STG.CRM.stg_EC_forecastdaily f on i.salesDocument = f.SOID and i.salesdocumentItem = f.SOItemNo  
 --left join EDW_ANALYTICS.dbo.forecast c on  c.OpportunityID = f.OpportunityID and c.OpportunityItemNo = f.OpportunityItemNo
 left join EDW_ANALYTICS.CRM.EC_dim_area_sales d on i.SalesmanID  = d.sales_id
 --left join EDW_ANALYTICS.CRM.EC_dim_area_store das on d.sales_code = das.sales_code
 left join EDW_ANALYTICS.CRM.EC_dim_company_exception_map dcm on dcm.CompanyID = CAST(i.SoldToParty as INT)
-left join logscore ls on ls.sernr = i.serialnumber and ls.billing_date = i.billingdate
+left join EDW_ANALYTICS_STG.CRM.stg_EC_scorelog ls on ls.sernr = i.serialnumber and ls.billing_date = i.billingdate
 left join EDW_ANALYTICS.CRM.EC_dim_customer_group_mapping da on i.SoldToParty = da.Account_ID
 
 where 1=1
@@ -475,8 +167,8 @@ select
 	da.Customer_Group
 	--,CONCAT(f.AccountID,i.ProductHierarchy, year(f.deliverydate), month(f.deliverydate)) AS forecastedkey
 
-from forecaseDaily f 
-left join invoice i on i.salesDocument = f.SOID and i.salesdocumentItem = f.SOItemNo 
+from EDW_ANALYTICS_STG.CRM.stg_EC_forecastdaily f 
+left join EDW_ANALYTICS_STG.CRM.stg_EC_ordertrackingPP i on i.salesDocument = f.SOID and i.salesdocumentItem = f.SOItemNo 
 --left join EDW_ANALYTICS.dbo.forecast c on c.OpportunityID = f.OpportunityID and c.OpportunityItemNo = f.OpportunityItemNo 
 left join EDW_ANALYTICS.CRM.EC_dim_area_store das on das.sales_code = f.SalesOffice
 left join EDW_ANALYTICS.CRM.EC_dim_company_exception_map dcm on dcm.CompanyID = f.AccountID
@@ -662,8 +354,8 @@ SELECT
 	,da.Customer_Group
 	--,CONCAT(f.AccountID,st3.ProductHierarchy, year(st3.SCORE_DATE), month(st3.SCORE_DATE)) AS forecastedkey
 
-FROM st3
-LEFT JOIN forecaseDaily AS f ON st3.VBELN_VA = f.SOID AND st3.POSNR_VA = f.SOItemNo
+FROM EDW_ANALYTICS_STG.CRM.stg_EC_ordertrackingrental st3
+LEFT JOIN EDW_ANALYTICS_STG.CRM.stg_EC_forecastdaily AS f ON st3.VBELN_VA = f.SOID AND st3.POSNR_VA = f.SOItemNo
 --LEFT JOIN EDW_ANALYTICS.dbo.forecast AS c ON c.OpportunityID = f.OpportunityID AND c.OpportunityItemNo = f.OpportunityItemNo
 LEFT JOIN EDW_ANALYTICS.CRM.EC_dim_area_store das ON das.sales_code = st3.sales_code
 left join EDW_ANALYTICS.CRM.EC_dim_customer_group_mapping da on st3.SOLD = da.Account_ID
@@ -848,8 +540,8 @@ SELECT
 	,da.Customer_Group
 	--,CONCAT(f.AccountID,st3.ProductHierarchy, year(f.deliverydate), month(f.deliverydate)) AS forecastedkey
 
-FROM forecaseDaily AS f
-LEFT JOIN st3 ON st3.VBELN_VA = f.SOID AND st3.POSNR_VA = f.SOItemNo
+FROM EDW_ANALYTICS_STG.CRM.stg_EC_forecastdaily AS f
+LEFT JOIN EDW_ANALYTICS_STG.CRM.stg_EC_ordertrackingrental st3 ON st3.VBELN_VA = f.SOID AND st3.POSNR_VA = f.SOItemNo
 --LEFT JOIN EDW_ANALYTICS.dbo.forecast AS c ON c.OpportunityID = f.OpportunityID AND c.OpportunityItemNo = f.OpportunityItemNo
 LEFT JOIN EDW_ANALYTICS.CRM.EC_dim_area_store das ON das.sales_code = f.SalesOffice
 left join EDW_ANALYTICS.CRM.EC_dim_customer_group_mapping da on concat('000',cast(f.AccountID as nvarchar(10))) = da.Account_ID
